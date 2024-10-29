545315215381045367679bf1fe3453cf3bbcbaff
bola666 <163007758+bolameiameiameia@users.noreply.github.com>
2024-10-29 08:25:07 -0300

    .

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
+document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Impede o envio padrão do formulário
+    alert("Obrigado por entrar em contato! Responderemos em breve.");
+});
+
 document.querySelectorAll("#galeria img").forEach(function(img) {
     img.addEventListener("click", function() {
-        this.style.backgroundColor = this.style.backgroundColor === 'lightyellow' ? '' : 'lightyellow';
+        this.style.border = this.style.border === '2px solid lightyellow' ? '' : '2px solid lightyellow';
     });
 });
