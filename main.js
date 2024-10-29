e92825e383d54a0b6a5a9d9eb772853550ebf711
bola666 <163007758+bolameiameiameia@users.noreply.github.com>
2024-10-29 08:48:49 -0300

    .

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
-document.querySelector("form").addEventListener("submit", function(event) {
-    event.preventDefault(); // Impede o envio padrão do formulário
-    alert("Obrigado por entrar em contato! Responderemos em breve.");
-});
+const botaoComprar = document.querySelectorAll('.comprar');
 
-document.querySelectorAll("#galeria img").forEach(function(img) {
-    img.addEventListener("click", function() {
-        this.style.border = this.style.border === '2px solid lightyellow' ? '' : '2px solid lightyellow';
+botaoComprar.forEach(botao => {
+    botao.addEventListener('click', () => {
+        alert('Produto adicionado ao carrinho!');
     });
 });
