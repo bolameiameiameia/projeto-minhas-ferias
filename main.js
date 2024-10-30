9e480297a9ffa74ebf6c8a5137e51f72bfb329f5
bola666 <163007758+bolameiameiameia@users.noreply.github.com>
2024-10-29 21:16:13 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
+const botaoComprar = document.querySelectorAll('.comprar');
 
+botaoComprar.forEach(botao => {
+    botao.addEventListener('click', () => {
+        alert('Produto adicionado ao carrinho!');
+    });
+});
